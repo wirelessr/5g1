8d4f886f356234a85e28888343d55af66147d07c
shunyi <M157q.tw@gmail.com>
2018-03-28 02:39:31 +0800

    Make flask can read config from envvar

--- app/app/main.py
+++ app/app/main.py
@@ -2,18 +2,17 @@ from flask import Flask
 from flask import render_template
 from flask_googlemaps import GoogleMaps
 
+
 app = Flask(__name__)
+app.config.from_envvar("CONF")
 
 
 @app.route("/")
 def index():
-    print(app.config)
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.config.from_envvar("CONF")
-
     GoogleMaps(app)
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=app.config["DEBUG"], port=80)
+    app.run(host='0.0.0.0', port=80)
