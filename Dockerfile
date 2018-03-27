500e720839d138e9d61d405ccd46532aa13359bd
shunyi <M157q.tw@gmail.com>
2018-03-28 02:17:23 +0800

    Set config path via ENV

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,6 @@ FROM tiangolo/uwsgi-nginx-flask:python3.6
 COPY ./requirements.txt /tmp
 RUN pip install -r /tmp/requirements.txt
 
+ENV CONF=/app/conf.py
+
 COPY ./app /app
--- app/app/main.py
+++ app/app/main.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask import render_template
-
+from flask_googlemaps import GoogleMaps
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    print(app.config)
     return render_template('index.html')
 
 
 if __name__ == "__main__":
+    app.config.from_envvar("CONF")
+
+    GoogleMaps(app)
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=app.config["DEBUG"], port=80)
