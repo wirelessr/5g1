dceae5d0b5d9562e778aa0c1e97576a950d5651c
ChunTing Wu <ifloveyou2008@gmail.com>
2018-04-15 21:37:45 +0800

    Retrieve images

--- 5g1/main.py
+++ 5g1/main.py
@@ -10,28 +10,44 @@ from constants import (
     API_URL, CONDITIONS, WEB_URL_FORMAT_STR, SETTINGS_PATH, HEADERS,
 )
 
+from html.parser import HTMLParser
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.img = None
+    def handle_starttag(self, tag, attrs):
+        if ('id', 'hid_imgArr') in attrs:
+            for attr, value in attrs:
+                if attr == 'value':
+                    self.img = value
 
 cache = set()
 
 
-def get_houses():
+def get_houses(config):
     logger.info('requests 591 API...')
     response = requests.get(API_URL, params=CONDITIONS, headers=HEADERS)
+    max_pages = int(config['default']['max_pages'])
+    logger.info('total {0}, and retrieve {1}'.format(response.json()['records'], max_pages*30))
+    
+    for i in range(max_pages):
+        CONDITIONS['firstRow'] = i*30
+        response = requests.get(API_URL, params=CONDITIONS, headers=HEADERS)
 
-    try:
-        data = response.json()['data']
-    except KeyError:
-        logger.debug("response.json()['data']: {}".format(response.json()['data']))
-        logger.error("Cannnot get data from response.json['data']")
-    except Exception:
-        logger.debug("response: {}".format(response.text))
-        raise
-    else:
-        houses = data.get('data', [])
-        logger.info(len(houses))
+        try:
+            data = response.json()['data']
+        except KeyError:
+            logger.debug("response.json()['data']: {}".format(response.json()['data']))
+            logger.error("Cannnot get data from response.json['data']")
+        except Exception:
+            logger.debug("response: {}".format(response.text))
+            raise
+        else:
+            houses = data.get('data', [])
+            logger.info(len(houses))
 
-        for house in houses:
-            yield house
+            for house in houses:
+                yield house
 
 
 def log_house_info(house):
@@ -46,16 +62,26 @@ def log_house_info(house):
     logger.info("租金：{} {}".format(house['price'], house['unit']))
     logger.info("坪數：{} 坪".format(house['area']))
     logger.info("格局：{}".format(house['layout']))
+    for url in house['img_url']:
+        logger.info("圖片：{}".format(url))
     logger.info("更新時間：{}".format(time.ctime(house['refreshtime'])))
     logger.info("\n")
 
+def get_img_url(house):
+    detail = WEB_URL_FORMAT_STR.format(house['post_id'])
+    response = requests.get(detail, headers=HEADERS)
+    m = MyHTMLParser()
+    m.feed(response.text)
+    url_list = [url.strip('"') for url in m.img.split(',')]
+    house['img_url'] = url_list
 
-def search_houses():
-    houses = get_houses()
+def search_houses(config):
+    houses = get_houses(config)
     for house in houses:
         if house['post_id'] in cache:
             continue
 
+        get_img_url(house)
         log_house_info(house)
         cache.update([house['post_id']])
 
@@ -65,7 +91,8 @@ def main():
     config.read(SETTINGS_PATH)
 
     while True:
-        search_houses()
+        search_houses(config)
+        print('cache size:', len(cache))
         time.sleep(int(config['default']['parse_interval_in_seconds']))
 
 
